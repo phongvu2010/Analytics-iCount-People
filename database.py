b5901b85db856e87ac8599c6891ac428867ed1e0
phongvu2010 <phongvu2010@gmail.com>
2023-04-16 21:28:41 +0700

    master

--- database.py
+++ database.py
@@ -1,12 +1,11 @@
 # Import needed libraries
 import pandas as pd
-# import pyodbc
 import os
 
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
-# from sqlalchemy.ext.declarative import declarative_base
-# from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 from urllib import parse
 
 load_dotenv(override = True)
@@ -16,29 +15,25 @@ db_name = os.environ.get('DB_NAME', 'statistic')
 db_user = parse.quote_plus(str(os.environ.get('DB_USER', 'postgres')))
 db_pass = parse.quote_plus(str(os.environ.get('DB_PASS', '0000')))
 
-DATABASE_URL = f'mssql+pyodbc://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}?driver=SQL Server'
+# DATABASE_URL = f'mssql+pyodbc://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}?driver=SQL Server'
+DATABASE_URL = f'postgresql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}'
 
 engine = create_engine(DATABASE_URL)
-# SessionLocal = sessionmaker(autocommit = False, autoflush = False, bind = engine)
-# Base = declarative_base()
+Base = declarative_base()
+Session = sessionmaker(autocommit = False, autoflush = False, bind = engine)
 
-# Dependency
-# def get_db():
-#     db = SessionLocal()
-#     try:
-#         yield db
-#     finally:
-#         db.close()
+def getSession():
+    session = Session()
+    try:
+        yield session
+    finally:
+        session.close()
 
 # Read Target data into a dataframe
-def get_data(engine, tblName):
-    target = pd.read_sql('SELECT * FROM "' + tblName + '"', engine)
+# def get_data(engine, tblName):
+#     target = pd.read_sql('SELECT * FROM "' + tblName + '"', engine)
 
-    return target
+#     return target
 
-# tblName = 'dbo.num_crowd'
-df = get_data(engine, 'num_crowd')
-
-
-
-print(df.head())
+# df = get_data(engine, 'num_crowd')
+# print(df.head())
--- models.py
+++ models.py
@@ -1,30 +1,112 @@
-
 from database import Base
 
-from sqlalchemy import Column, ForeignKey, BigInteger, Float, Date, String, Boolean
+from sqlalchemy import Column, ForeignKey, Integer, SMALLINT, CHAR, NCHAR, BOOLEAN, DateTime
 from sqlalchemy.orm import relationship
 from sqlalchemy.schema import PrimaryKeyConstraint
 
+class Store(Base):
+    __tablename__ = 'store'
+    tid = Column(Integer, primary_key = True)
+    country = Column(CHAR(20))
+    area = Column(CHAR(20))
+    province = Column(CHAR(20))
+    city = Column(CHAR(20))
+    name = Column(CHAR(80), nullable = False)
+    address = Column(CHAR(80), nullable = False)
+    isbranch = Column(CHAR(3))
+    code = Column(CHAR(32), nullable = False)
+    cameranum = Column(Integer, nullable = False)
+    manager = Column(CHAR(20))
+    managertel = Column(CHAR(20))
+    lastEditDate = Column(DateTime)
+    formula = Column(CHAR(64))
+
 class Camera(Base):
     __tablename__ = 'camera'
-    tid = Column(String(15), primary_key = True)
-    storeid = Column(String)
-    DeviceID = Column(String(10), nullable = False)
-    address = Column(Boolean)
-    date
-    status
+    tid = Column(Integer, primary_key = True)
+    storeid = Column(Integer, ForeignKey('store.tid'), nullable = False)
+    DeviceID = Column(SMALLINT)
+    address = Column(CHAR(50), nullable = False)
+    date = Column(DateTime)
+    status = Column(Integer)
 
-class Historical(Base):
-    __tablename__ = 'historicals'
-    symbol = Column(String, ForeignKey('companies.symbol'))
-    date = Column(Date)
-    open = Column(Float, nullable = False)
-    high = Column(Float, nullable = False)
-    low = Column(Float, nullable = False)
-    close = Column(Float, nullable = False)
-    volume = Column(BigInteger, nullable = False)
+    store = relationship(Store)
 
-    stocks = relationship(Company)
+class NumCrowd(Base):
+    __tablename__ = 'num_crowd'
+    recordtime = Column(DateTime)
+    in_num = Column(Integer, nullable = False)
+    out_num = Column(Integer, nullable = False)
+    position = Column(CHAR(30))
+    storeid = Column(Integer, ForeignKey('store.tid'), nullable = False)
+
+    store = relationship(Store)
     __table_args__ = (
-        PrimaryKeyConstraint(symbol, date),
+        PrimaryKeyConstraint(recordtime, in_num, out_num, storeid),
     )
+
+class ErrLog(Base):
+    __tablename__ = 'ErrLog'
+    ID = Column(Integer, primary_key = True)
+    storeid = Column(Integer, ForeignKey('store.tid'), nullable = False)
+    DeviceCode = Column(SMALLINT)
+    LogTime = Column(DateTime)
+    Errorcode = Column(Integer)
+    ErrorMessage = Column(NCHAR(120))
+
+    store = relationship(Store)
+
+class Status(Base):
+    __tablename__ = 'Status'
+    ID = Column(Integer, primary_key = True)
+    storeid = Column(Integer, ForeignKey('store.tid'), nullable = False)
+    FlashNum = Column(Integer)
+    RamNum = Column(Integer)
+    RC1 = Column(BOOLEAN)
+    RC2 = Column(BOOLEAN)
+    RC3 = Column(BOOLEAN)
+    RC4 = Column(BOOLEAN)
+    RC5 = Column(BOOLEAN)
+    RC6 = Column(BOOLEAN)
+    RC7 = Column(BOOLEAN)
+    RC8 = Column(BOOLEAN)
+    DcID = Column(SMALLINT)
+    FV = Column(NCHAR(20))
+    DcTime = Column(DateTime)
+    RC1 = Column(BOOLEAN)
+    DeviceID = Column(SMALLINT)
+    IA = Column(Integer)
+    OA = Column(Integer)
+    S = Column(SMALLINT)
+    T = Column(DateTime)
+
+    store = relationship(Store)
+
+class Setting(Base):
+    __tablename__ = 'setting'
+    tid = Column(Integer, primary_key = True)
+    companyname = Column(CHAR(50), nullable = False)
+    companyaddress = Column(CHAR(128))
+    companytel = Column(CHAR(20))
+    companyip = Column(CHAR(50), nullable = False)
+    setupdate = Column(DateTime)
+    setupname = Column(CHAR(50))
+    setupaddress = Column(CHAR(50))
+    setuptel = Column(CHAR(50))
+
+class User(Base):
+    __tablename__ = 'users'
+    tid = Column(Integer, primary_key = True)
+    name = Column(CHAR(20), nullable = False)
+    password = Column(CHAR(32), nullable = False)
+    realname = Column(CHAR(50), nullable = False)
+    sex = Column(CHAR(6))
+    tel = Column(CHAR(20))
+    address = Column(CHAR(128))
+    country = Column(CHAR(20))
+    area = Column(CHAR(20))
+    province = Column(CHAR(20))
+    city = Column(CHAR(20))
+    storeid = Column(Integer, ForeignKey('store.tid'))
+
+    store = relationship(Store)
