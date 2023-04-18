c95be0bbbcd3c6743928d4db1d327ebbe49bb189
phongvu2010 <phongvu2010@gmail.com>
2023-04-19 00:45:42 +0700

    master

--- refesh_data.py
+++ refesh_data.py
@@ -1,8 +1,8 @@
-# import database as db
-import pandas as pd
+import database as db
+# import pandas as pd
 
-# from models import Store, NumCrowd, ErrLog, Status, Setting
+from models import Store, NumCrowd, ErrLog, Status, Setting
 
 def dbSetting():
-    # return db.getSession().query(Setting).first()
-    return pd.read_feather('temp/Setting.feather')
+    return db.getSession().query(Setting).first()
+    # return pd.read_feather('temp/Setting.feather')
