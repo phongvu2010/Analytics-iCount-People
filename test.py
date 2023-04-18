21c1995c8fdb6aefb407fb5e9e026617b0321db3
phongvu2010 <phongvu2010@gmail.com>
2023-04-18 12:56:22 +0700

    Merge branch 'master' of https://github.com/phongvu2010/icount

--- test.py
+++ test.py
@@ -1,11 +1,13 @@
 import database as db
-import models as m
 import pandas as pd
 
 from database import engine, getSession
 from models import Store, Camera, NumCrowd, ErrLog, Status, Setting, User
 
-results = db.getSession().query(User).all() #.filter(m.Store.id>2)
-df = pd.DataFrame([r._asdict() for r in results])
-df.to_csv('temp/User.csv')
-print(df)
+# results = db.getSession().query(Setting).all() #.filter(m.Store.id>2)
+# df = pd.DataFrame([r._asdict() for r in results])
+# df.to_feather('temp/Setting.feather')
+
+df = pd.read_feather('temp/NumCrowd.feather')
+
+print(df.head())
