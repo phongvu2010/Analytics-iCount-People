186ebfe8ac36c6dcb64489677c954febd779b013
phongvu2010 <phongvu2010@gmail.com>
2023-04-19 18:05:05 +0700

    master

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 # streamlit run main.py
-# import refesh_data as db
+
+import numpy as np
+import pandas as pd
+import refesh_data as db
 import streamlit as st
 import streamlit_authenticator as stauth
 import yaml
 
-from st_pages import Page, show_pages#, add_page_title
+from datetime import date
 from yaml.loader import SafeLoader
 
 # Basic Page Configuration
@@ -12,17 +15,17 @@ from yaml.loader import SafeLoader
 st.set_page_config(
     page_title = 'Main - People Counting System',
     page_icon = '📈',
-    layout = 'centered'
+    layout = 'wide'
 )
 
 # hashed_passwords = stauth.Hasher(['admin', 'user']).generate()
 # print(hashed_passwords)
 
-with open('.streamlit/config.yaml') as file:
-    config = yaml.load(file, Loader = SafeLoader)
-
 if 'authentication_status' in st.session_state:
     st.session_state['authentication_status'] = ''
+
+with open('.streamlit/config.yaml') as file:
+    config = yaml.load(file, Loader = SafeLoader)
 
 authenticator = stauth.Authenticate(
     config['credentials'],
@@ -33,44 +36,61 @@ authenticator = stauth.Authenticate(
 )
 
 name, authentication_status, username = authenticator.login('Login', 'main')
-
-def create_pages_link():
-    l = []
-    l.append(Page('main.py', 'Home', '🏠'))
-    if st.session_state['name'] == 'Admin':
-        l.append(Page('pages/errLog.py', 'Error Log', ':books:'))
-
-    return l
-
-# Optional -- adds the title and icon to the current page
-# add_page_title()
-
-# Specify what pages should be shown in the sidebar, and what their titles and icons should be
-show_pages(create_pages_link())
-
 authen_status = st.session_state['authentication_status']
-if authen_status is False:
-    st.error('Username/password is incorrect')
-elif authen_status is None:
-    st.warning('Please enter your username and password')
 
 @st.cache_resource
 def getSetting():
-    return None
-    # return db.dbSetting()
+    return db.dbSetting()
+
+@st.cache_resource
+def getNumCrowd():
+    return db.dbNumCrowd()
+
+def getWeekNums(year):
+    start_date = '1/1/' + year
+    end_date = '31/12/' + year
+    data = pd.date_range(start = start_date, end = end_date, freq = 'D')
+
+    df = pd.DataFrame(data, columns = ['date'])
+
+    df['w'] = df['date'].dt.strftime('%V').astype(int)
+    df['s'] = df['w'].shift(1)
+    df['week_num'] = np.where(df['s'].isna(), 1, df['w'] + 1)
+    group = df.groupby('week_num').agg({'date': ['min', 'max']}).reset_index()
+    group['week'] = 'WK' + group['week_num'].astype(str) + \
+                    ' (' + group['date']['min'].dt.strftime('%d/%m') + \
+                    ' - ' + group['date']['max'].dt.strftime('%d/%m') + ')'
+
+    return group['week'].to_list()
 
 if authen_status:
     db_setting = getSetting()
+elif authen_status is False:
+    st.error('Username/password is incorrect')
 
+if authen_status:
     with st.sidebar:
         st.header(f'Welcome *{st.session_state["name"]}*')
 
-        # st.write(db_setting.companyname)
-        # st.write(db_setting.companyaddress)
-        # st.write(db_setting.companytel)
+        st.write(db_setting.companyname)
+        st.write(db_setting.companyaddress)
+        st.write(db_setting.companytel)
 
         authenticator.logout('Logout', 'main')
 
     with st.container():
-        st.header('Information')
+        st.header('Statistics report')
+
+        top_menu = st.columns(4)
+        with top_menu[0]:
+            option = st.selectbox('How would you like to be statistics?', ('Daily', 'Weekly', 'Monthly', 'Quarter', 'Yearly'))
+
+        with top_menu[1]:
+            if option == 'Daily':
+                d = st.date_input('Date', date.today())
+            elif option == 'Weekly':
+                select_year = st.selectbox('Year', reversed(range(2018, date.today().year + 1)))
+                d = st.selectbox('Week', getWeekNums(str(select_year)))
+
         st.write('The average number is according to every store by every day/week/month/year.')
+        st.write('Your birthday is:', d)
