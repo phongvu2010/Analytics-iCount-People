9463dec8f3d6871dd355f0144d0402fc23e8d88f
phongvu2010 <phongvu2010@gmail.com>
2023-04-21 03:23:24 +0700

    master

--- refesh_data.py
+++ refesh_data.py
@@ -1,24 +1,29 @@
-import database as db
-# import pandas as pd
+# import database as db
+import pandas as pd
 
-from models import Store, NumCrowd, ErrLog, Status, Setting
+# from models import Store, NumCrowd, ErrLog, Status, Setting
 
 def dbErrLog():
-    return db.getSession().query(ErrLog).all()
-    # return pd.read_feather('temp/ErrLog.feather')
+    # results = db.getSession().query(ErrLog).all()
+    # return pd.DataFrame([r._asdict() for r in results])
+    return pd.read_feather('temp/ErrLog.feather')
 
 def dbNumCrowd():
-    return db.getSession().query(NumCrowd).all()
-    # return pd.read_feather('temp/NumCrowd.feather')
+    # results = db.getSession().query(NumCrowd).all()
+    # return pd.DataFrame([r._asdict() for r in results])
+    return pd.read_feather('temp/NumCrowd.feather')
 
 def dbSetting():
-    return db.getSession().query(Setting).first()
-    # return pd.read_feather('temp/Setting.feather')
+    # results = db.getSession().query(Setting).first()
+    # return pd.DataFrame([r._asdict() for r in results])
+    return pd.read_feather('temp/Setting.feather')
 
 def dbStatus():
-    return db.getSession().query(Status).all()
-    # return pd.read_feather('temp/Status.feather')
+    # results = db.getSession().query(Status).all()
+    # return pd.DataFrame([r._asdict() for r in results])
+    return pd.read_feather('temp/Status.feather')
 
 def dbStore():
-    return db.getSession().query(Store).all()
-    # return pd.read_feather('temp/Store.feather')
+    # results = db.getSession().query(Store).all()
+    # return pd.DataFrame([r._asdict() for r in results])
+    return pd.read_feather('temp/Store.feather')
