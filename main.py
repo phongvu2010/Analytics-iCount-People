25687d7484f40c82fffa856ffd06bdc71a384ecf
phongvu2010 <phongvu2010@gmail.com>
2023-04-21 03:23:24 +0700

    master

--- main.py
+++ main.py
@@ -6,24 +6,22 @@ import numpy as np
 import pandas as pd
 import refesh_data as db
 import mockup_page as mp
-from datetime import date
+
+from datetime import date, datetime
 
 mp.set_pages()
 mp.hidden_style()
 
 authen = mp.login()
 authen_status = st.session_state['authentication_status']
-# username = st.session_state['username']
 
 @st.cache_resource
 def getStore():
-    results = db.dbStore()
-    return pd.DataFrame([r._asdict() for r in results])
+    return db.dbStore()
 
 @st.cache_resource
 def getNumCrowd():
-    results = db.dbNumCrowd()
-    return pd.DataFrame([r._asdict() for r in results])
+    return db.dbNumCrowd()
 
 def getWeekNums(year):
     start_date = '1/1/' + year
@@ -40,23 +38,41 @@ def getWeekNums(year):
                     ' (' + group['date']['min'].dt.strftime('%d/%m') + \
                     ' - ' + group['date']['max'].dt.strftime('%d/%m') + ')'
 
-    return group['week'].to_list()
+    return group
 
-if authen_status:
-    db_store = getStore()
-elif authen_status is False:
-    st.error('Username/password is incorrect')
+def filter_data(data, store = 0, d = None, w = None, m = None, q = None, y = None):
+    if (not y) and d:
+        # print('Daily')
+        data = data[data.recordtime.dt.date == d]
+    else:
+        data = data[data.recordtime.dt.year == y]
+        if w:
+            # print('Weekly')
+            data = data[data.recordtime.dt.date >= datetime.fromtimestamp(w[0]) & data.recordtime.dt.date <= datetime.fromtimestamp(w[1])]
+        elif m:
+            # print('Monthly')
+            data = data[data.recordtime.dt.strftime('%B') == m]
+        elif q:
+            # print('Quarter')
+            data = data[data.recordtime.dt.to_period('Q').dt.strftime('%q').astype(int) == q + 1]
+
+    if store > 0:
+        data = data[data.storeid == store]
+
+    return data
 
 if authen_status:
     mp.sidebar_info(authen)
+    db_store = getStore()
 
     with st.container():
-        st.header('STATISTICS REPORT')
+        st.header('statistics report'.title())
 
         top_menu = st.columns(4)
         with top_menu[0]:
-            l_store = [''] + db_store['name'].to_list()
-            store_selected = st.selectbox('Store:', l_store)
+            display = tuple([''] + db_store['name'].to_list())
+            options = list(range(len(display)))
+            store_selected = st.selectbox('Store:', options, format_func = lambda x: display[x])
 
         with top_menu[1]:
             option = st.selectbox('By:', ('Daily', 'Weekly', 'Monthly', 'Quarter', 'Yearly'), index = 2)
@@ -67,24 +83,36 @@ if authen_status:
                 d = st.date_input('Date:', date.today())
             elif option == 'Weekly':
                 y = st.selectbox('Year:', reversed(range(2018, date.today().year + 1)))
-                with top_menu[3]: w = st.selectbox('Week:', getWeekNums(str(y)))
+                with top_menu[3]:
+                    weeks = getWeekNums(str(y))
+                    display = tuple(weeks['week'])
+                    options = list(range(len(display)))
+                    w = st.selectbox('Week:', options, format_func = lambda x: display[x])
+                    w = (weeks.loc(0)[w][1], weeks.loc(0)[w][2])
             elif option == 'Monthly':
                 y = st.selectbox('Year:', reversed(range(2018, date.today().year + 1)))
                 with top_menu[3]: m = st.selectbox('Month:', calendar.month_name[1:], index = date.today().month - 1)
             elif option == 'Quarter':
                 y = st.selectbox('Year', reversed(range(2018, date.today().year + 1)))
-                with top_menu[3]: q = st.selectbox('Quarter:', (1, 2, 3, 4), index = (date.today().month - 1) // 3)
+                display = ('Spring', 'Summer', 'Autumn', 'Winter')
+                options = list(range(len(display)))
+                print(display)
+                print(options)
+                with top_menu[3]: q = st.selectbox('Quarter:', options, format_func = lambda x: display[x], index = (date.today().month - 1) // 3)
             else:
                 y = st.selectbox('Year:', reversed(range(2018, date.today().year + 1)))
 
         st.write('The average number is according to every store by every day/week/month/year.')
-        # st.write('Your birthday is:', d)
-        print(f'{store_selected} - {type(store_selected)}')
-        
-        print(f'{d} - {type(d)}')
-        print(f'{w} - {type(w)}')
-        print(f'{m} - {type(m)}')
-        print(f'{q} - {type(q)}')
-        print(f'{y} - {type(y)}')
-        print('-----')
-        
+
+        data = filter_data(getNumCrowd().copy(), db_store.loc[store_selected - 1, 'tid'], d, w, m, q, y)
+        st.write(data)
+
+        # st.write(f'Store : { store_selected } - { type(store_selected) }')        
+        # if store_selected > 0:
+        #     st.write(db_store.loc[store_selected - 1, 'tid'])
+
+        # st.write(f'Daily : { d } - { type(d) }')
+        # st.write(f'Weekly : { w } - { type(w) }')
+        # st.write(f'Monthly : { m } - { type(m) }')
+        # st.write(f'Quarter : { q } - { type(q) }')
+        # st.write(f'Yearly : { y } - { type(y) }')
