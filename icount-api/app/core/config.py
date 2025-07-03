fa8266ec3109ecea7ceeb1434d598531c6ab9cbb
phongvu2010 <phongvu2010@gmail.com>
2025-07-03 22:03:58 +0700

    master

--- icount-api/app/core/config.py
+++ icount-api/app/core/config.py
@@ -1,37 +1,53 @@
-from pydantic import computed_field
-from pydantic_core import MultiHostUrl
+from pydantic import AnyUrl, BeforeValidator, computed_field
+# from pydantic_core import MultiHostUrl
 from pydantic_settings import BaseSettings
-from urllib import parse
+from typing import Annotated, Any
+# from urllib import parse
 
+
+def parse_cors(v: Any) -> list[str] | str:
+    if isinstance(v, str) and not v.startswith("["):
+        return [i.strip() for i in v.split(",")]
+    elif isinstance(v, list | str):
+        return v
+    raise ValueError(v)
 
 class Settings(BaseSettings):
-    SECRET_KEY: str
-    ALGORITHM: str
-    ACCESS_TOKEN_EXPIRE_MINUTES: int
+    API_VERSION: str = "/api/v1"
+    PROJECT_NAME: str
+    DESCRIPTION: str
 
-    # --- Cấu hình cho MSSQL ---
-    MSSQL_HOST: str
-    MSSQL_PORT: int = 1433  # Cổng mặc định của MSSQL
-    MSSQL_USER: str
-    MSSQL_PASSWORD: str
-    MSSQL_DB: str
-    # MSSQL_DRIVER: str = 'ODBC Driver 17 for SQL Server'
-    MSSQL_DRIVER: str = 'SQL Server'
+    BACKEND_CORS_ORIGINS: Annotated[
+        list[AnyUrl] | str, BeforeValidator(parse_cors)
+    ] = []
 
-    @computed_field  # type: ignore[misc]
-    @property
-    def SQLALCHEMY_DATABASE_URI(self) -> str:
-        return str(MultiHostUrl.build(
-            scheme = 'mssql+pyodbc',
-            username = self.MSSQL_USER,
-            password = parse.quote_plus(self.MSSQL_PASSWORD),
-            host = self.MSSQL_HOST,
-            port = self.MSSQL_PORT,
-            path = self.MSSQL_DB,
-            query = f'driver={self.MSSQL_DRIVER}'
-        ))
+#     SECRET_KEY: str
+#     ALGORITHM: str
+#     ACCESS_TOKEN_EXPIRE_MINUTES: int
+
+#     # --- Cấu hình cho MSSQL ---
+#     MSSQL_HOST: str
+#     MSSQL_PORT: int = 1433  # Cổng mặc định của MSSQL
+#     MSSQL_USER: str
+#     MSSQL_PASSWORD: str
+#     MSSQL_DB: str
+#     # MSSQL_DRIVER: str = 'ODBC Driver 17 for SQL Server'
+#     MSSQL_DRIVER: str = 'SQL Server'
+
+#     @computed_field  # type: ignore[misc]
+#     @property
+#     def SQLALCHEMY_DATABASE_URI(self) -> str:
+#         return str(MultiHostUrl.build(
+#             scheme = 'mssql+pyodbc',
+#             username = self.MSSQL_USER,
+#             password = parse.quote_plus(self.MSSQL_PASSWORD),
+#             host = self.MSSQL_HOST,
+#             port = self.MSSQL_PORT,
+#             path = self.MSSQL_DB,
+#             query = f'driver={self.MSSQL_DRIVER}'
+#         ))
 
     class Config:
         env_file = '.env'
 
-settings = Settings()
+# settings = Settings()
--- icount-api/app/main.py
+++ icount-api/app/main.py
@@ -1,15 +1,30 @@
 from fastapi import FastAPI
-from .api.routers import data, logs, auth
+from starlette.middleware.cors import CORSMiddleware
 
-from .core.config import settings
+# from .api.routers import data, logs, auth
+from core.config import settings
 
 app = FastAPI(
-    title='iCount People API',
-    description='API for analyzing people traffic in stores.',
-    version='1.1.0'
+    title = settings.PROJECT_NAME,
+    description = settings.DESCRIPTION,
+    openapi_url = f"{settings.API_VERSION}/openapi.json",
+    version = "1.1.0"
 )
 
+# Set all CORS enabled origins
+if settings.BACKEND_CORS_ORIGINS:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins = [
+            str(origin).strip("/") for origin in settings.BACKEND_CORS_ORIGINS
+        ],
+        allow_credentials = True,
+        allow_methods = ["*"],
+        allow_headers = ["*"]
+    )
+
 # # Thêm các router vào ứng dụng với tiền tố /api/v1
+# app.include_router(api_router, prefix = settings.API_V1_STR)
 # app.include_router(auth.router, prefix='/api/v1', tags=['Authentication'])
 # app.include_router(data.router, prefix='/api/v1', tags=['Crowd Data'])
 # app.include_router(logs.router, prefix='/api/v1', tags=['Error Logs'])
