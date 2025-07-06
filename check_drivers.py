ba258824bd0d42e7319628f3d22abec4fad93364
phongvu2010 <phongvu2010@gmail.com>
2025-07-07 01:44:45 +0700

    master

--- check_drivers.py
+++ check_drivers.py
@@ -4,10 +4,10 @@ try:
     # Lấy danh sách tất cả các driver
     installed_drivers = pyodbc.drivers()
 
-    print("✅ Các ODBC driver đã được cài đặt trên máy:")
+    print('✅ Các ODBC driver đã được cài đặt trên máy:')
 
     # In ra từng driver cho dễ nhìn
     for driver in installed_drivers:
         print(driver)
 except Exception as ex:
-    print(f"Lỗi khi liệt kê driver: {ex}")
+    print(f'Lỗi khi liệt kê driver: {ex}')
