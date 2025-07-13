ca17892bc339ea044120b6f84f946df388f27946
Phong Vu 2010 <phongvu2010@gmail.com>
2025-07-14 01:55:26 +0700

    master

---
+++ app/schemas/errors.py
@@ -0,0 +1,12 @@
+# from pydantic import BaseModel
+# from datetime import datetime
+
+# class ErrorLog(BaseModel):
+#     id: str
+#     store_name: str
+#     log_time: datetime
+#     error_code: int
+#     error_message: str
+
+#     class Config:
+#         from_attributes = True
